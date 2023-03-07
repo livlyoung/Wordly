bf982bf2249c7b28fb9e56acd25fb1e33d99c027
Liv Young <liv@dhcp-155-68-162-79.fandm.edu>
2023-03-06 21:14:46 -0500

    Added MainActivity

---
+++ app/src/main/java/edu/fandm/teamyellowstone/wordly/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fandm.teamyellowstone.wordly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
